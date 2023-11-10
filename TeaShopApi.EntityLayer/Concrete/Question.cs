580f0c8e10a2821964447c34778414863ab4d7e6
aydinmfatih <aydinmfatih@gmail.com>
2023-11-10 22:15:02 +0300

    Add project files.

---
+++ TeaShopApi.EntityLayer/Concrete/Question.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaShopApi.EntityLayer.Concrete
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string QuestionDetail { get; set; }
+        public string AnswerDetail { get; set; }
+    }
+}
+    
---
+++ TeaShopApi.EntityLayer/Concrete/Testimonial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeaShopApi.EntityLayer.Concrete
+{
+    public class Testimonial
+    {
+        public int TestimonialId { get; set; }
+        public string TestimonialName { get; set; }
+        public string TestimonialComment { get; set; }
+        public string TestimonialImageUrl { get; set; }
+
+    }
+}
