4ddb02611f790c48ff7db1d042210298fda78099
aydinmfatih <aydinmfatih@gmail.com>
2023-11-30 00:51:54 +0300

    updated

--- TeaShopApi.DataAccessLayer/Context/TContext.cs
+++ TeaShopApi.DataAccessLayer/Context/TContext.cs
@@ -1,12 +1,24 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TeaShopApi.EntityLayer.Concrete;
 
 namespace TeaShopApi.DataAccessLayer.Context
 {
-    public class TContext
+    public class TeaContext:DbContext
     {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=MSI\\SQLEXPRESS;initial Catalog=TeaShopDb;integrated Security=true;");
+
+
+        }
+        public DbSet<Drink> Drinks { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Testimonial> Testimonials { get; set; }
     }
 }
