0051260a0360aec99aae13eb0c4ad35ea87f0c47
aydinmfatih <aydinmfatih@gmail.com>
2023-12-01 20:00:11 +0300

    updated

--- TeaShopApi.WebUI/Areas/Admin/Controllers/DrinksController.cs
+++ TeaShopApi.WebUI/Areas/Admin/Controllers/DrinksController.cs
@@ -58,5 +58,33 @@ namespace TeaShopApi.WebUI.Areas.Admin.Controllers
 			}
 			return View();
 		}
+
+		[HttpGet]
+		public async Task<IActionResult> UpdateDrink(int id)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7059/api/Drinks/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+               var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<UpdateDrinkDto>(jsonData);
+				return View(values);
+            }
+			return View();
+        }
+
+		[HttpPost]
+		public async Task<IActionResult> UpdateDrink(UpdateDrinkDto updateDrinkDto)
+		{
+			var client = _httpClientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(updateDrinkDto);
+			StringContent content = new StringContent(jsonData,Encoding.UTF8,"application/json");
+			var responseMessage = await client.PutAsync("https://localhost:7059/api/Drinks/", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+			return View();
+        }
 	}
 }
---
+++ TeaShopApi.WebUI/Dtos/DrinkDtos/UpdateDrinkDto.cs
@@ -0,0 +1,10 @@
+﻿namespace TeaShopApi.WebUI.Dtos.DrinkDtos
+{
+    public class UpdateDrinkDto
+    {
+        public int drinkId { get; set; }
+        public string drinkName { get; set; }
+        public decimal drinkPrice { get; set; }
+        public string drinkImageUrl { get; set; }
+    }
+}
